75555c404a0cdd5baee94cc38b807208488938f7
sunimalherath <sunimaldv@gmail.com>
2020-09-20 17:20:13 +1000

    Initial Commit

---
+++ OBJC_Hello/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OBJC_Hello
+//
+//  Created by Sunimal Herath on 20/9/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OBJC_Hello/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  OBJC_Hello
+//
+//  Created by Sunimal Herath on 20/9/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

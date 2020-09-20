d3550711251fb1b496003939c5ddd1340765a2c9
sunimalherath <sunimaldv@gmail.com>
2020-09-20 19:23:52 +1000

    adding alert view with functionality

--- OBJC_Hello/ViewController.m
+++ OBJC_Hello/ViewController.m
@@ -18,5 +18,11 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)clickBaitPressed:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Welcome" message:@"Welcome to the world of Objective-C" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:alertAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
